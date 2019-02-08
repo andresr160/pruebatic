a460c1633c1d9f9d5ef840fb978691b9cf5dfd06
Andres Rueda <afruedag@hotmail.com>
2019-02-08 10:04:52 -0500

    Creacion de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'registro', loadChildren: './pages/registro/registro.module#RegistroPageModule' },
+  { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule' },
+  { path: 'discos', loadChildren: './pages/discos/discos.module#DiscosPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/discos/discos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discos',
+  templateUrl: './discos.page.html',
+  styleUrls: ['./discos.page.scss'],
+})
+export class DiscosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
