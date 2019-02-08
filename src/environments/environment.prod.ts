a3d59b84f19a343a370575a6d6251529b71427b3
Andres Rueda <afruedag@hotmail.com>
2019-02-08 11:26:39 -0500

    se importaron los paquetes de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCuNoxY2hBmJPdq0VK0RrmA2vrnTKeHoj4',
+    authDomain: 'pruebatic-5f58a.firebaseapp.com',
+    databaseURL: 'https://pruebatic-5f58a.firebaseio.com',
+    projectId: 'pruebatic-5f58a',
+    storageBucket: 'pruebatic-5f58a.appspot.com',
+    messagingSenderId: '91776215766'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCuNoxY2hBmJPdq0VK0RrmA2vrnTKeHoj4',
+    authDomain: 'pruebatic-5f58a.firebaseapp.com',
+    databaseURL: 'https://pruebatic-5f58a.firebaseio.com',
+    projectId: 'pruebatic-5f58a',
+    storageBucket: 'pruebatic-5f58a.appspot.com',
+    messagingSenderId: '91776215766'
+  }
 };
 
 /*
